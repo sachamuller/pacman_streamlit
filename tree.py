e0cc8ddbd70025eaf18af8f3771b369f8b9da624
Sacha Muller <chloe.muller@student-cs.fr>
2022-11-28 21:10:53 +0100

    can plot a tree in plotly with generic names

--- tree.py
+++ tree.py
@@ -1,5 +1,6 @@
 from enum import Enum
-from utils import NodeType
+from utils import NodeTypes
+from plot_tree import plot_tree
 
 
 class Tree:
@@ -8,57 +9,58 @@ class Tree:
         self.list_nodes = []
         self.max_depth = None
 
-        self.build_list_nodes()
-        self.get_max_depth()
-
-    def fill_nodes_coordinates(self):
-        bottom_layer = self.get_nodes_with_same_depth(self.max_depth)
-        bottom_layer_per_parents = self.group_nodes_per_parents(bottom_layer)
-        x = 0
-        for parent in bottom_layer_per_parents.keys():
-            for node in bottom_layer_per_parents[parent]:
-                node.x_coordinate = x
-                x += 1
-        # damn it's hard to plot a tree ??
-
-    def build_list_nodes(self):
+    def convert_to_dict_list(self):
+        self.initial_node.name = 0
+        current_name = 1
+        result = {0: []}
         nodes_to_expand = [self.initial_node]
         while len(nodes_to_expand) > 0:
             expanded_node = nodes_to_expand.pop()
-            self.list_nodes.append(expanded_node)
+            if len(expanded_node.children_nodes) > 0:
+                for child_node in expanded_node.children_nodes:
+                    child_node.name = current_name
+                    current_name += 1
+                result[expanded_node.name] = [
+                    child.name for child in expanded_node.children_nodes
+                ]
             nodes_to_expand += expanded_node.children_nodes
-
-    def get_nodes_with_same_depth(self, depth):
-        result = []
-        for node in self.list_nodes:
-            if node.depth == depth:
-                result.append(node)
-        return result
-
-    def get_max_depth(self):
-        for node in self.list_nodes:
-            if self.max_depth == None or node.depth > self.max_depth:
-                self.max_depth = node.depth
-
-    def group_nodes_per_parents(self, nodes_sublist):
-        result = {}
-        for node in nodes_sublist:
-            if node.parent_node in result:
-                result[node.parent_node].append(node)
-            else:
-                result[node.parent_node] = [node]
         return result
 
 
 class Node:
     def __init__(self, value, parent_node, from_action, node_type, children_nodes=[]):
         self.value = value
-        self.parent_node = parent_node
         self.from_action = from_action
         self.children_nodes = []
-        self.depth = parent_node.depth + 1
-        self.x_coordinate = None
         self.node_type = node_type
+        self.name = None
+
+        self.parent_node = parent_node
+        if parent_node != None:  # only for initial node in theory
+            self.parent_node.add_child_node(self)
+            self.depth = parent_node.depth + 1
+        else:
+            self.depth = 0
 
     def add_children_nodes(self, children):
         self.children_nodes += children
+
+    def add_child_node(self, child):
+        self.children_nodes.append(child)
+
+    def update_value(self, new_value):
+        self.value = new_value
+
+
+if __name__ == "__main__":
+    a = Node(1, None, None, NodeTypes.max)
+    b = Node(2, a, None, NodeTypes.max)
+    c = Node(3, a, None, NodeTypes.max)
+    d = Node(4, b, None, NodeTypes.max)
+    e = Node(5, b, None, NodeTypes.max)
+    f = Node(6, b, None, NodeTypes.max)
+    f = Node(7, e, None, NodeTypes.max)
+
+    tree = Tree(a)
+    dict_list = tree.convert_to_dict_list()
+    plot_tree(dict_list)
